4a74b25f54cfa18ab4bdb72f229e86cf75b11553
Daniel Zimmerman <dzimmerm@mail.umw.edu>
2012-02-16 17:00:50 -0500

    Changed name of file to ensure it pulls up without prompting

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>UMW Textbook Swap</title>
+<link rel="stylesheet" href="styles.css" type="text/css" />
+
+
+</head>
+<body>
+
+<div id="wrap">
+	<div class="pagewrapper">
+		<div class="innerpagewrapper">
+			<div class="page">
+				<div class="content">
+				
+					<!-- CONTENT -->
+					<h3>UMW Textbook Swap</h3>
+					<p>Welcome to the official textbook swap website at the University of Mary Washington! </p>
+					
+					<form method = "post" action = "report2.php">
+					<table>										<tr><td>&nbsp;</td><td><input type="submit" value="Login" /></td></tr>
+					</table>
+					
+					</form>
+<form method = "post" action = "report2.php">
+					<table>										<tr><td>&nbsp;</td><td><input type="submit" value="Register" /></td></tr>
+					</table>
+					
+					</form>
+
+					<!-- END CONTENT -->
+					
+				</div>				   
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
+
