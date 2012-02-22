5250af38c1bea8622030864c17169437308d0fc0
dpeworch <dpeworch@mail.umw.edu>
2012-02-22 17:57:02 -0500

    added format

--- index.php
+++ index.php
@@ -1,30 +1,28 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+	session_start();
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>UMW Textbook Swap</title>
-<link rel="stylesheet" href="styles.css" type="text/css" />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Home Page </title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+
 
 
 </head>
 <body>
 
-<div id="wrap">
-	<div class="pagewrapper">
-		<div class="innerpagewrapper">
-			<div class="page">
-				<div class="content">
-				
+<div id="contents">				
 					<!-- CONTENT -->
 					<h3>UMW Textbook Swap</h3>
 					<p>Welcome to the official textbook swap website at the University of Mary Washington! </p>
 					
-					<form method = "post" action = "report2.php">
+					<form method = "post" action = "login.php">
 					<table>										<tr><td>&nbsp;</td><td><input type="submit" value="Login" /></td></tr>
 					</table>
 					
 					</form>
-<form method = "post" action = "report2.php">
+<form method = "post" action = "register.php">
 					<table>										<tr><td>&nbsp;</td><td><input type="submit" value="Register" /></td></tr>
 					</table>
 					
@@ -32,11 +30,6 @@
 
 					<!-- END CONTENT -->
 					
-				</div>				   
-			</div>
-		</div>
-	</div>
-</div>
-</body>
+				</div>
+	</body>
 </html>
-
