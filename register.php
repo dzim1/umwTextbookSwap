c1129b78a4acdfa00e71cb5a13b2d93b4b29799a
pgalyen <pdg@pdg-PC.(none)>
2012-02-22 23:41:41 -0500

    registration page -needs work

---
+++ register.php
@@ -0,0 +1,48 @@
+<?php
+	session_start();
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>New User Page </title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+	<body>
+	<div id="contents">
+		
+
+
+	<form method = "post" action = "register2.php">
+					<table>
+					<tr><td>User Name</td><td><input type="text" id="username" name="username" /></td></tr>
+					<tr><td>Password</td><td><input type="text" id="password" name="password" /></td></tr>
+					<tr><td>School Email</td><td><input type="text" id="email" name="email" /></td></tr>
+				    <tr><td>Phone Number</td><td><input type="text" id="phonenumber" name="phonenumber" /></td></tr>
+					<tr><td>&nbsp;</td><td><input type="submit" value="Register" /></td></tr>
+					</table>
+					
+					</form>		
+	
+		<?php
+			include "db_connect.php";
+			include "BookExchange.sql";
+			
+			
+			
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+			$email = $_POST['email'];
+			$phonenumber = $_POST['phonenumber'];
+			
+			
+			$insertInto = "INSERT INTO user VALUES('$username','$password','$email','$phonenumber',0,0,0)";
+			$insertIntoQuery = mysqli_query($db, $insertInto);
+	
+					
+?>
+		
+	</div>
+	</body>
+</html>
+	
