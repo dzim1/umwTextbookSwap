a2eed142e862d0a5738065373d69d35ba5af892a
Daniel Zimmerman <dzimmerm@mail.umw.edu>
2012-02-26 18:01:25 -0500

    New homepage format

--- index.php
+++ index.php
@@ -1,35 +1,32 @@
-<?php
-	session_start();
-?>
+<?php session_start(); ?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>Home Page </title>
   <link rel="stylesheet" type="text/css" href="style.css" />
 
-
-
 </head>
 <body>
 
 <div id="contents">				
-					<!-- CONTENT -->
-					<h3>UMW Textbook Swap</h3>
-					<p>Welcome to the official textbook swap website at the University of Mary Washington! </p>
-					
-					<form method = "post" action = "login.php">
-					<table>										<tr><td>&nbsp;</td><td><input type="submit" value="Login" /></td></tr>
-					</table>
-					
-					</form>
-<form method = "post" action = "createAccount.php">
-					<table>										<tr><td>&nbsp;</td><td><input type="submit" value="Register" /></td></tr>
-					</table>
-					
-					</form>
+<!-- CONTENT -->
+	<h3>UMW Textbook Swap</h3>
+	<p>Welcome to the official textbook swap website at the University of Mary Washington! </p>
+			
+	<form method = "post" action = "login.php">
+	<table>
+	<tr><td>&nbsp;</td><td><input type="submit" value="Login" /></td>
+		
+	</form>
+	<form method = "post" action = "createAccount.php">
+	<td>&nbsp;</td><td><input type="submit" value="Register" /></td>
 
-					<!-- END CONTENT -->
-					
-				</div>
-	</body>
+	<form method="post" action="search.php" /></td>
+	<td>&nbsp;</td><td><input type="submit" value="Search" /></td>
+				
+	</form>
+	<!-- END CONTENT -->
+				
+</div>
+</body>
 </html>
