6fe72645a5fd5e41d09e9c00b2227ada9b1c5638
Edward Martinez <edmz99@yahoo.com>
2012-03-12 22:44:27 -0700

    added logout

---
+++ logout.php
@@ -0,0 +1,20 @@
+<?php session_start(); ?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Log Out </title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+
+</head>
+<body>
+	<?php
+		session_destroy();
+	?>
+	
+	<div id="contents">
+		<h3> You have been logged out </h3>
+		<p><a href="index.php">Go back to Home Page</a></p>
+	</div>
+	
+</body>
+</html>
